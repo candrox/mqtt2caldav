dfa4b8b152cb84f16bc1d9ef91afbdb3aaec0048
candrox <107208579@mail.com>
2025-04-12 12:33:47 +0800

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -2,16 +2,26 @@
 import os
 import json
 
+
+
+### SECTION :: Application Name ##########################################################
+APP_NAME = "MQTT2CALDAV"
+
+
+
 ### SECTION :: Path Definitions ##########################################################
 _cur_dir = os.path.dirname(os.path.realpath(__file__))
 ROOT_DIR = os.path.join(_cur_dir, os.pardir)
 LOG_DIR = os.path.join(ROOT_DIR, 'logs')
 LOG_FILE = "mqtt2caldav.log"
-CONF_DIR = os.path.join(ROOT_DIR, 'config')
-CONF_FILE = "config.json"
+CONF_DIR_NAME = "config"
+CONF_FILE_NAME = "config.json"
+CONF_DIR = os.path.join(ROOT_DIR, CONF_DIR_NAME)
+CONF_FILE = CONF_FILE_NAME
 
 
-### SECTION :: JSON Constants ############################################################
+
+### SECTION :: Config File Constants #####################################################
 with open(os.path.join(CONF_DIR, CONF_FILE)) as json_file:
     json_data = json.load(json_file)
     MQTT_SERVER_ADDRESS = json_data['MQTT_SERVER']['MQTT_SERVER_ADDRESS']
