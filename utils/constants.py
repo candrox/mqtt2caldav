391bfc675dccfdc56aa5cf284484bc78c5285422
candrox <107208579@mail.com>
2025-09-02 11:13:04 +0800

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,5 @@
 ### SECTION :: Module Imports ############################################################
 import os
-import json
 
 
 
@@ -12,25 +11,13 @@ APP_NAME = "MQTT2CALDAV"
 ### SECTION :: Path Definitions ##########################################################
 _cur_dir = os.path.dirname(os.path.realpath(__file__))
 ROOT_DIR = os.path.join(_cur_dir, os.pardir)
+
 LOG_DIR = os.path.join(ROOT_DIR, 'logs')
-LOG_FILE = "mqtt2caldav.log"
-CONF_DIR_NAME = "config"
-CONF_FILE_NAME = "config.json"
-CONF_DIR = os.path.join(ROOT_DIR, CONF_DIR_NAME)
-CONF_FILE = CONF_FILE_NAME
+LOG_FILE_NAME = "mqtt2caldav.log"
+LOCK_FILE_NAME = "mqtt2caldav.lock"
+LOCK_FILE_PATH = os.path.abspath(os.path.join(LOG_DIR, LOCK_FILE_NAME))
 
-
-
-### SECTION :: Config File Constants #####################################################
-with open(os.path.join(CONF_DIR, CONF_FILE)) as json_file:
-    json_data = json.load(json_file)
-    MQTT_SERVER_ADDRESS = json_data['MQTT_SERVER']['MQTT_SERVER_ADDRESS']
-    MQTT_SERVER_PORT = json_data['MQTT_SERVER']['MQTT_SERVER_PORT']
-    MQTT_USERNAME = json_data['MQTT_SERVER']['MQTT_USERNAME']
-    MQTT_PASSWORD = json_data['MQTT_SERVER']['MQTT_PASSWORD']
-
-    CALDAV_SERVER_ADDRESS = json_data['CALDAV_SERVER']['CALDAV_SERVER_ADDRESS']
-    CALDAV_USERNAME = json_data['CALDAV_SERVER']['CALDAV_USERNAME']
-    CALDAV_PASSWORD = json_data['CALDAV_SERVER']['CALDAV_PASSWORD']
-
-    TRIGGERS = json_data['TRIGGERS']
+CONFIG_DIR_NAME = "config"
+SETTINGS_FILE_NAME = "settings.json"
+TRIGGERS_FILE_NAME = "triggers.json"
+CONFIG_DIR = os.path.join(ROOT_DIR, CONFIG_DIR_NAME)
