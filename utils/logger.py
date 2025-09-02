15871b3fa0d5a36c7a0aede08b2f6eb4959a8bd5
candrox <107208579@mail.com>
2025-09-02 11:13:25 +0800

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,15 +1,15 @@
 ### SECTION :: Module Imports ############################################################
-import logging
-import sys
 import os
-from utils.constants import LOG_DIR, LOG_FILE
+import sys
+import logging
+from utils.constants import LOG_DIR, LOG_FILE_NAME, APP_NAME
+
 
 
 ### SECTION :: Logfile Path ##############################################################
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR, exist_ok=True)
-
-log_fn = os.path.join(LOG_DIR, LOG_FILE)
+log_fn = os.path.join(LOG_DIR, LOG_FILE_NAME)
 
 
 
@@ -25,58 +25,69 @@ class LowercaseLevelFormatter(logging.Formatter):
         else:
             levelname = levelname.lower()
 
-        levelname = f"{levelname:<5}"
-        record.levelname_padded = levelname
-
+        record.levelname_padded = f"{levelname:<5}"
         return super().format(record)
 
 
 
-###  SECTION :: Formatter And Logger Configuration #######################################
+### SECTION :: Logger And Handler Configuration ##########################################
 formatter = LowercaseLevelFormatter(
-    fmt='%(levelname_padded)s %(asctime)s: %(message)s', # Use padded levelname_padded
-    datefmt='%Y/%m/%d %H:%M:%S') # Bracket moved to this line
+	# Alternative to add milliseconds to time-stamps
+    # fmt='%(levelname_padded)s %(asctime)s.%(msecs)03d: %(message)s',
+    fmt='%(levelname_padded)s %(asctime)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s: %(message)s',
-                    datefmt='%Y/%m/%d %H:%M:%S',
-                    filename=log_fn)
+# Get Application Logger
+logger = logging.getLogger(APP_NAME)
+logger.setLevel(logging.INFO)
+
+# Create File Handler
+file_handler = logging.FileHandler(log_fn)
+file_handler.setFormatter(formatter)
+
+# Create Stream Handler
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setFormatter(formatter)
+
+# Add Handlers To Logger
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+logger.propagate = False
 
 
-
-###  SECTION :: Logger And Handler Configuration #########################################
-logger = logging.getLogger('MQTT-CALDAV')
-
-root_logger = logging.getLogger()
-if root_logger.handlers:
-    handler = root_logger.handlers[0]
-    handler.setFormatter(formatter)
-else:
-    print("[SYS] No handler found for root logger. Custom formatter might not be applied to file logging.")
-
-
-
-### SECTION :: Log Message Output Function ###############################################
-def __log_msg(msg):
-    sys.stdout.write(msg + '\n')
-    if len(msg) > 0 and msg[0] == '\r':
-        msg = msg[1:]
-    return msg
-
+### FUNCTION :: Set Log Level From Config ################################################
+def set_log_level(level_str: str):
+    """Sets the logger level based on a string name."""
+    level_map = {
+        'DEBUG': logging.DEBUG,
+        'INFO': logging.INFO,
+        'WARN': logging.WARNING,
+        'WARNING': logging.WARNING,
+        'ERROR': logging.ERROR,
+        'CRIT': logging.CRITICAL,
+        'CRITICAL': logging.CRITICAL
+    }
+    log_level = level_map.get(level_str.upper(), logging.INFO)
+    logger.setLevel(log_level)
+    if level_str.upper() not in level_map and level_str:
+         warn_formatter = logging.Formatter(fmt='%(asctime)s.%(msecs)03d', datefmt='%Y-%m-%d %H:%M:%S')
+         timestamp = warn_formatter.format(logging.LogRecord(name=APP_NAME, level=logging.WARNING, pathname='', lineno=0, msg='', args=[], exc_info=None, func=''))
+         print(f"warn  {timestamp}: [APP] Invalid LOG_LEVEL set, defaulting to INFO.")
 
 
 ### SECTION :: Level-Based Logging Functions #############################################
 def info(msg):
-    logger.info(__log_msg(msg))
+    logger.info(msg)
 
 def warn(msg):
-    logger.warning(__log_msg(msg))
+    logger.warning(msg)
 
 def error(msg):
-    logger.error(__log_msg(msg))
+    logger.error(msg)
 
 def debug(msg):
-    logger.debug(__log_msg(msg))
+    logger.debug(msg)
 
 def critical(msg):
-    logger.critical(__log_msg(msg))
+    logger.critical(msg)
